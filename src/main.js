e0b73a087950d474d628a841f4bdbbfed80ea9f4
ggbondfucker <904983850@qq.com>
2021-01-06 22:41:16 +0800

    改良1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+//图片切换
+const $touchlogo=$('.touchlogo')
+const $baidu_big=$('.baidu_big')
+const $searchlogo=$('.searchlogo')
+
+//其他功能
 const $siteList=$('.siteList')
 const $lastLi= $siteList.find('li.last') 
 const x= localStorage.getItem('x')
 const xObject =JSON.parse(x)//把字符串变成对象
 const hashMap = xObject || [
-    {logo:'A',logoType:'text',url:'http://www.acfun.cn'},
-    {logo:'B',logoType:'image',url:'https://www.bilibili.com'}
+    {logo:'D',logoSrc:'images/douyu.png',logoType:'image',url:'http://www.douyu.com'},
+    {logo:'B',logoSrc:'images/bilibili.png',logoType:'image',url:'https://www.bilibili.com'}
 ]
 const simplifyUrl=(url)=>{
     return url.replace('https://','')
@@ -12,13 +18,21 @@ const simplifyUrl=(url)=>{
     .replace('www.','')
     .replace(/\/.*/,'') //清除/开头的内容
 }
+const isimage=(logoType,node)=>{
+    if(logoType==='image'){
+        return '<img src="'+`${node.logoSrc}`+'"alt="">'
+        
+    }else{
+        return `${node.logo[0]}`
+    }
+}
 const render=()=>{
     $siteList.find('li:not(.last)').remove()
     hashMap.forEach((node,index)=>{
         const $li =$(`
         <li>
           <div class="site">
-            <div class="logo">${node.logo[0]}</div>
+            <div class="logo">${isimage(node.logoType,node)}</div>
             <div class="link">${simplifyUrl(node.url)}</div>
             <div class="close">
                 <svg class="icon">
@@ -54,10 +68,10 @@ hashMap.push({
     render()
 })
 
-$(window).on('beforeunload',()=>{
-    const string =JSON.stringify(hashMap) //把对象变成字符串
-    localStorage.setItem('x',string)
-})
+// $(window).on('beforeunload',()=>{
+//     const string =JSON.stringify(hashMap) //把对象变成字符串
+//     localStorage.setItem('x',string)
+// })
 
 $(document).on('keypress',(e)=>{
     const key =e.key
@@ -66,4 +80,18 @@ $(document).on('keypress',(e)=>{
             window.open(node.url)
         }
     })
+})
+
+
+$touchlogo.on('click',()=>{
+    
+    $touchlogo.find('.baidu').fadeToggle(0).siblings('.google').fadeToggle(0)
+    $searchlogo.find('.baidu_big').fadeToggle(0).siblings('.google_big').fadeToggle(0)
+    if($('.baidu').css('display')!=='none'){
+        $('.searchForm').prop('action','https://www.baidu.com/s')
+    }else{
+        $('.searchForm').prop('action','https://www.google.com/search?')
+        
+    }
+    console.log($('.searchForm').prop('action'))
 })
